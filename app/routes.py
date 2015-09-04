dd4b1e16a73edfea849bc3bc498898a7eaeee893
Matthew Cornell <matt@matthewcornell.org>
2015-09-04 16:06:34 -0400

    added example starting points from news

--- app/routes.py
+++ app/routes.py
@@ -13,9 +13,21 @@ def index():
     return render_template('index.html')
 
 
-# analysis starting point
+# collect list of example links to analyze
+@app.route('/seeds/<path:url>')
+def seeds_for_aggregator_url(url):
+    try:
+        html_analysis = HtmlAnalysis(url)
+        # outgoing_links = html_analysis.outgoing_links(True)
+        # [print('[{}]({})'.format(l.text, l.get('href'))) for l in outgoing_links]
+        return render_template("seeds.html", html_analysis=html_analysis)
+    except Exception as e:
+        return "error rendering URL: '" + url + "': " + str(e)
+
+
+# analysis starting point for a single url
 @app.route('/analysis/<path:url>')
-def url_analysis_start(url):
+def analysis_start(url):
     try:
         html_analysis = HtmlAnalysis(url)
         return render_template("analysis.html", html_analysis=html_analysis)
@@ -45,4 +57,4 @@ def empty_analysis():
 def do_url_submit():
     url = request.values.get('url_field_val', None)
     # todo urllib.parse.quote(url)?
-    return redirect(url_for('url_analysis_start', url=url))
+    return redirect(url_for('analysis_start', url=url))
